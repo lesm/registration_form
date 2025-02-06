f50116b9267218ec387e7b86fb76e1249f4e0a82
Luis Silva <silmar.dll@gmail.com>
2025-02-06 12:26:23 -0600

    Includes date and time to the screenshots

--- fill_form.rb
+++ fill_form.rb
@@ -4,18 +4,22 @@ browser = Watir::Browser.new :chrome
 
 browser.goto ENV.fetch('FORM_URL', '')
 
+# Find form
 form = browser.form(id: ENV.fetch('FORM_ID', ''))
 
+# Fill form inputs
 form.text_field(index: 0).set(ENV.fetch('EMPLOYEE_NUMBER', ''))
 form.text_field(index: 1).set(ENV.fetch('EMPLOYEE_NAME', ''))
 
+date_and_time = Time.now.getlocal('-06:00').strftime('%Y-%m-%d-%H:%S')
+
 # Take a screenshot before submission
-browser.screenshot.save('before_submission.png')
+browser.screenshot.save("before_submission_#{date_and_time}.png")
 
 # Submit the form
 # browser.button(type: 'submit').click
 
 # Take a screenshot after submission
-# browser.screenshot.save('after_submission.png')
+browser.screenshot.save("after_submission_#{date_and_time}.png")
 
 browser.close
