7d125107b845703a4080a052e0c8ba65deec2e5c
Luis Silva <silmar.dll@gmail.com>
2025-02-18 10:03:04 -0600

    rescue UnknownObjectException and TimeoutError Watir exceptions

--- run.rb
+++ run.rb
@@ -1,13 +1,26 @@
 require './web_automation'
 require './telegram_bot'
 
-telegram_bot = TelegramBot.new
+def fill_form
+  web_automation.fill_form
+rescue Watir::Exception::UnknownObjectException, Watir::Wait::TimeoutError
+  sleep(2)
+  web_automation.fill_form
+end
+
+def telegram_bot
+  @telegram_bot ||= TelegramBot.new
+end
+
+def web_automation
+  @web_automation ||= WebAutomation.new
+end
 
 if (1..5).cover?(Time.now.getlocal('-06:00').wday)
-  web_automation = WebAutomation.new
-  web_automation.fill_form
-
+  # weekdays process
+  fill_form
   telegram_bot.send_evidences(web_automation.screenshots_names)
 else
+  # weekend process
   telegram_bot.send_weekend_message
 end
